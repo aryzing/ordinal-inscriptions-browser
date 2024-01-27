0560c5c81e8a65b83ab614c616ec848fd904aa15
Eduard Bardají Puig <ebardaji@gmail.com>
2024-01-27 21:28:30 +0100

    Remove query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   RouterProvider,
   createBrowserRouter,
@@ -46,7 +45,6 @@ export default function App() {
           </div>
         </div>
       </div>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
